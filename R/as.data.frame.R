aa6585614e6af7e725b81b9064402a672e95a111
Patrick Reidy <patrick.francis.reidy@gmail.com>
2016-09-29 17:43:30 -0500

    Fixed argument lists of  methods to agree with the generic

--- R/as.data.frame.R
+++ R/as.data.frame.R
@@ -12,6 +12,13 @@ NULL
 #' and points that have contentful, non-empty labels are returned after coercion.
 #'
 #' @param x An \code{IntervalTier}, \code{PointTier}, or \code{TextGrid} object.
+#' @param row.names \code{NULL} or a character vector giving the row names for
+#'   the returned \code{data.frame}. If \code{NULL} (the default), the rows
+#'   of the returned \code{data.frame} are named numerically beginning at 1.
+#' @param optional A logical, default is \code{FALSE}.
+#'   If \code{TRUE}, setting row names and converting column names is optional.
+#' @param ... Additional optional arguments. (Only here for consistency with
+#'   the generic.)
 #' @param stringsAsFactors A logical, default is \code{FALSE}.
 #' @return A \code{data.frame} object whose rows represent the contentful
 #'   intervals or points within \code{object}, and whose columns are:
@@ -24,7 +31,7 @@ NULL
 
 #' @rdname textgRid-as.data.frame
 #' @export
-as.data.frame.IntervalTier <- function(x, stringsAsFactors = FALSE) {
+as.data.frame.IntervalTier <- function(x, row.names = NULL, optional = FALSE, ..., stringsAsFactors = FALSE) {
   .intervals <- findIntervals(tier = x, stringsAsFactors = stringsAsFactors)
   .tier_data <- data.frame(
     TierNumber = tierNumber(x),
@@ -36,14 +43,18 @@ as.data.frame.IntervalTier <- function(x, stringsAsFactors = FALSE) {
     .tier_data[rep(1, times = nrow(.intervals)), ],
     .intervals
   )
-  row.names(.coerced) <- 1:nrow(.coerced)
+  if (is.null(row.names)) {
+    row.names(.coerced) <- 1:nrow(.coerced)
+  } else {
+    row.names(.coerced) <- row.names
+  }
   return(.coerced)
 }
 
 
 #' @rdname textgRid-as.data.frame
 #' @export
-as.data.frame.PointTier <- function(x, stringsAsFactors = FALSE) {
+as.data.frame.PointTier <- function(x, row.names = NULL, optional = FALSE, ..., stringsAsFactors = FALSE) {
   .points <- with(findPoints(tier = x), {
     data.frame(
       Index     = Index,
@@ -63,18 +74,26 @@ as.data.frame.PointTier <- function(x, stringsAsFactors = FALSE) {
     .tier_data[rep(1, times = nrow(.points)), ],
     .points
   )
-  row.names(.coerced) <- 1:nrow(.coerced)
+  if (is.null(row.names)) {
+    row.names(.coerced) <- 1:nrow(.coerced)
+  } else {
+    row.names(.coerced) <- row.names
+  }
   return(.coerced)
 }
 
 
 #' @rdname textgRid-as.data.frame
 #' @export
-as.data.frame.TextGrid <- function(x, stringsAsFactors = FALSE) {
+as.data.frame.TextGrid <- function(x, row.names = NULL, optional = FALSE, ..., stringsAsFactors = FALSE) {
   .coerced <- Reduce(
     rbind,
     Map(f = as.data.frame, x = x, stringsAsFactors = list(stringsAsFactors))
   )
-  row.names(.coerced) <- 1:nrow(.coerced)
+  if (is.null(row.names)) {
+    row.names(.coerced) <- 1:nrow(.coerced)
+  } else {
+    row.names(.coerced) <- row.names
+  }
   return(.coerced)
 }

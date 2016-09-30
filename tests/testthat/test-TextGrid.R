2e6bfa218ec9ebfdfecb1dcea8c416fcd6bf8b20
Patrick Reidy <patrick.francis.reidy@gmail.com>
2016-09-29 19:11:22 -0500

    Better testing for TextGrid() constructor

--- tests/testthat/test-TextGrid.R
+++ tests/testthat/test-TextGrid.R
@@ -3,15 +3,35 @@ context('TextGrid')
 
 
 test_that('TextGrid() instantiates a TextGrid object', {
-  .textgrid <- TextGrid('../test.TextGrid')
-  expect_s4_class(object = .textgrid, class = 'TextGrid')
+  .textgrid_file <- '../test.TextGrid'
+  expect_s4_class(
+    object = TextGrid(.textgrid_file),
+    class  = 'TextGrid'
+  )
+  expect_s4_class(
+    object = TextGrid(readLines(.textgrid_file)),
+    class  = 'TextGrid'
+  )
 })
 
 
 test_that('TextGrid accessors return slot-values', {
-  .textgrid <- TextGrid('../test.TextGrid')
-  expect_equal(object   = textGridStartTime(.textgrid),
-               expected = .textgrid@startTime)
-  expect_equal(object   = textGridEndTime(.textgrid),
-               expected = .textgrid@endTime)
+  .textgrid_file <- '../test.TextGrid'
+  expect_equal(
+    object   = textGridStartTime(TextGrid(.textgrid_file)),
+    expected = TextGrid(.textgrid_file)@startTime
+  )
+  expect_equal(
+    object   = textGridStartTime(TextGrid(readLines(.textgrid_file))),
+    expected = TextGrid(readLines(.textgrid_file))@startTime
+  )
+  expect_equal(
+    object   = textGridEndTime(TextGrid(.textgrid_file)),
+    expected = TextGrid(.textgrid_file)@endTime
+  )
+  expect_equal(
+    object   = textGridEndTime(TextGrid(readLines(.textgrid_file))),
+    expected = TextGrid(readLines(.textgrid_file))@endTime
+  )
 })
+

16959f1b67ccbe1dd787ff6fc2e734f6ea667eb7
Jonathan Washington <jonathan.n.washington@gmail.com>
2021-06-28 11:35:17 -0400

    support for extra level of indentation in tier information

--- R/IntervalTier-utilities.R
+++ R/IntervalTier-utilities.R
@@ -1,20 +1,20 @@
 
 # Extract the start times of the intervals within a block of @praatText.
-.IntervalStartTimes <- function(praatText, pattern = '^( {12}|\t{3})xmin') {
+.IntervalStartTimes <- function(praatText, pattern = '^( {12}|\t{34})xmin') {
   .start_times <- .TierTimes(praatText, pattern)
   return(.start_times)
 }
 
 
 # Extract the end times of the intervals within a block of @praatText.
-.IntervalEndTimes <- function(praatText, pattern = '^( {12}|\t{3})xmax') {
+.IntervalEndTimes <- function(praatText, pattern = '^( {12}|\t{34})xmax') {
   .end_times <- .TierTimes(praatText, pattern)
   return(.end_times)
 }
 
 
 # Extract the text-mark values (interval labels) from a block of @praatText.
-.IntervalLabels <- function(praatText, pattern = '^( {12}|\t{3})(text|mark)') {
+.IntervalLabels <- function(praatText, pattern = '^( {12}|\t{34})(text|mark)') {
   .labels <- .TierLabels(praatText, pattern)
   return(.labels)
 }

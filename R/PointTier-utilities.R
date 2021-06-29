6fe3224b852641d018d3356cd131bff2c2f406a1
Jonathan Washington <jonathan.n.washington@gmail.com>
2021-06-29 12:54:18 -0400

    read point tiers with either 3 or 4 tabs

--- R/PointTier-utilities.R
+++ R/PointTier-utilities.R
@@ -1,13 +1,13 @@
 
 # Extract the times of the points within a block of @praatText.
-.PointTimes <- function(praatText, pattern = '^( {12}|\t{3})(time|number)') {
+.PointTimes <- function(praatText, pattern = '^( {12}|\t{3,4})(time|number)') {
   .times <- .TierTimes(praatText, pattern)
   return(.times)
 }
 
 
 # Extract the labels of the points within a block of @praatText.
-.PointLabels <- function(praatText, pattern = '^( {12}|\t{3})(text|mark)') {
+.PointLabels <- function(praatText, pattern = '^( {12}|\t{3,4})(text|mark)') {
   .labels <- .TierLabels(praatText, pattern)
   return(.labels)
 }
